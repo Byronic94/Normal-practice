22baeb5f5e97bfedd34631c96d92873cd0a5acb8
Byronic94 <yanyan090123@gmail.com>
2015-07-27 19:02:48 +0800

    change the client.js

--- client.js
+++ client.js
@@ -3,10 +3,14 @@ var message = new Buffer("你好！");
 var client = dgram.createSocket("udp4");
 client.send(message,0,message.length,41234,"localhost",function(err,bytes){
 	if(err) console.log('发送数据失败。');
-	else console.log("已发送%d字节数据。"，bytes);
+	else console.log("已发送%d字节数据。",bytes);
 });
 client.on("message", function(msg,rinfo) {
 	console.log("已接收服务端发出的数据:%s"+msg);
 	console.log("服务端地址为%s",rinfo.address);
 	console.log("服务端所用端口为%s.",rinfo.port);
+	client.close();
+});
+client.on("close",function(){
+	console.log("socket端口被关闭");
 });
