521e692fb92091ad043f815b82495d52c514cbc3
Byronic94 <yanyan090123@gmail.com>
2015-07-27 18:38:43 +0800

    node.js udp server

---
+++ server.js
@@ -0,0 +1,13 @@
+var dgram = require("dgram");
+var server = dgram.createSocket("udp4");
+server.on("message", function(msg,rinfo) {
+	console.log("已接收客户端发出的数据"+msg);
+	console.log("客户端地址信息为%j",rinfo);
+	var buf = new Buffer("确认信息："+msg);
+	server.send(buf,0,buf.length,rinfo.port,rinfo.address);
+});
+server.on("listening",function(){
+	var address = server.address();
+	console.log("服务器开始监听。地址信息为%j",address);
+});
+server.bind(41234,'localhost');
